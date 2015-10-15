5febdf65d7bbb400f13e3f31abd16c62db855bf7
dellch <cdell@flmnh.ufl.edu>
2015-10-15 00:05:23 -0400

    used CONSTANT and added get_totals_by transcription_center

--- scripts/get_totals.rb
+++ scripts/get_totals.rb
@@ -1,5 +1,6 @@
 require_relative "cartodb"
+require_relative "constants"
 
-cartodb = CartoDB.new("wdb")
+cartodb = CartoDB.new(TABLE_NAME)
 cartodb.get_totals
 cartodb.get_totals_by "transcription_center"
