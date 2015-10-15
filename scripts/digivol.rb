41555a1e5df0fc227ef3efc82433dd57ef68df3f
dellch <cdell@flmnh.ufl.edu>
2015-10-14 21:44:46 -0400

    digivol script

---
+++ scripts/digivol.rb
@@ -0,0 +1,48 @@
+require_relative 'cartodb'
+require 'json'
+require 'open-uri'
+
+begin
+  table_name = 'digivol'
+  transcription_center = 'DigiVol'
+  digivol_url = "http://volunteer-dev.ala.org.au/ws/wedigbio"
+  response = open(digivol_url).read
+  values = []
+  unless response.empty?
+    json = JSON.parse(response)
+    json.each do |item|
+      transcription_id = item["transcription_id"]
+      project = item["project_name"]
+      user_id = item["user_id"]
+      user_ip_address = item["user_ip_address"]
+      subject_id = ""
+      specimen_url = item["specimen_url"]
+      specimen_image_url = item["specimen_image_url"][0].to_s
+      transcription_timestamp = Time.at(item["transcription_timestamp"]/1000).to_s
+      transcribed_country = item["transcribed_country"][0].to_s
+      transcribed_state = item["transcribed_state"][0].to_s
+      transcribed_county = item["transcribed_county"][0].to_s
+      transcribed_species = item["transcribed_species"][0].to_s
+      values.push([
+                      transcription_id,
+                      transcription_center,
+                      project,
+                      user_id,
+                      user_ip_address,
+                      subject_id,
+                      specimen_url,
+                      specimen_image_url,
+                      transcription_timestamp,
+                      transcribed_country,
+                      transcribed_state,
+                      transcribed_county,
+                      transcribed_species,
+                      Time.now.to_s
+                  ])
+    end
+    cartodb = CartoDB.new table_name
+    cartodb.insert values
+  end
+rescue
+
+end
